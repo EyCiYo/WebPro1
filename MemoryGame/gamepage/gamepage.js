1573f432e4a83ac8e853f03377ef5af3bcd21d6a
EyCiYo <alexchdn12@gmail.com>
2023-10-02 14:50:32 +0530

    More game logic implemented

--- MemoryGame/gamepage/gamepage.js
+++ MemoryGame/gamepage/gamepage.js
@@ -2,8 +2,8 @@ let result = [];
 let answer = [];
 let gamestat = true;
 let score = 0;
-let stage = 4;
-let matsize = 4;
+let stage = 0;
+let matsize = 0;
 
 function sleep(ms) 
 {
@@ -22,58 +22,6 @@ function disableListener()
     cards.forEach(card => card.disabled = true);
 }
 
-function gameEnd()
-{
-    gamestat = false;
-    console.log("U failed");
-    disableListener();
-}
-
-function init() {
-  makeMat(matsize);
-}
-init();
-gameloop();
-function gameloop()
-{
-    setEventListener();
-    result = [];
-    while(gamestat)
-    {
-        answer = getRandomList(stage,0,matsize*matsize);
-        illuminate(answer);
-        gamestat = false;
-    }
-}
-async function illuminate(answer)
-{
-    disableListener();
-    await sleep(500);
-    for(let i = 0; i < stage; i++)
-    {
-        //code to sequentially illuminate the cards in answer array
-        document.getElementById(answer[i]).style.backgroundColor = "red";
-        await sleep(500);  
-        document.getElementById(answer[i]).style.backgroundColor = "#64CCC5";     
-    }
-    setEventListener(); 
-}
-function checkResult(result, length)
-{
-    if(result[length-1] != answer[length-1])
-    {
-        gameEnd();
-    }
-    else if(length == stage)
-    {
-        score++;
-        stage++;
-        gamestat = true;
-        result = [];
-        answer = [];
-        gameloop();
-    }
-}
 function getRandomList(length, min, max)
 {
     const list = [];
@@ -84,18 +32,98 @@ function getRandomList(length, min, max)
     return list;
 }
 
-function handleClick(event) {
-    let card = event.target;
-    let cardId = card.id;
-    if(cardId in answer === false || result.length > stage)
+function gameEnd()
+{
+    gamestat = false;
+    console.log("U failed");
+    disableListener();
+    document.getElementById('enddiag').showModal();
+}
+
+function init() {
+    score = 0;
+    stage = 1;
+    matsize = 3;
+    result = [];
+    answer = [];
+    makeMat(matsize);
+    gameloop();
+}
+init();
+document.getElementById('restartbtn').addEventListener('click', () => {
+    document.getElementById('enddiag').close();
+    init();
+});
+function gameloop()
+{
+    result = [];
+    console.log(result);
+    setEventListener();
+    if(gamestat)
+    {
+        answer = getRandomList(stage,0,matsize*matsize);
+        illuminate(answer);
+    }
+}
+async function lightUp(cardId)
+{
+    document.getElementById(cardId).style.backgroundColor = "red";
+    await sleep(500);  
+    document.getElementById(cardId).style.backgroundColor = "#64CCC5"; 
+}
+
+async function illuminate(answer)
+{
+    disableListener();
+    await sleep(500);
+    for(let i = 0; i < stage; i++)
+    {
+        //code to sequentially illuminate the cards in answer array
+        await lightUp(answer[i]);   
+    }
+    setEventListener(); 
+}
+function checkResult(result, length)
+{
+    console.log(result,length);
+    if(result[length-1] != answer[length-1])
     {
         gameEnd();
     }
-    else
+    else if(length == stage)
     {
+        score++;
+        stage++;
+        document.getElementById('score').innerHTML = "Score: "+ score;
+        if(stage%5 == 0)
+        {
+            matsize++;
+            makeMat(matsize);
+        }
+        gamestat = true;
+        result = [];
+        answer = [];
+        gameloop();
+    }
+}
+
+
+async function handleClick(event) {
+    let card = event.target;
+    let cardId = Number(card.id);
+    await lightUp(cardId);
+    if(answer.includes(cardId))
+    {
+        console.log(result);
+        console.log(cardId);
         result.push(cardId);    
         checkResult(result,result.length);
     }
+    else
+    {
+        gameEnd();
+    }
+
 }
 function makeMat(val){
     document.querySelector('.matrix').innerHTML='';
